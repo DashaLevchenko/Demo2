d479f03aaab5d6adef845d6b042e2eb363eadb8a
Dasha Levchenko <dashulalevchenko@gmail.com>
2020-03-16 18:32:24 +0200

    Add method to increase or decrease fields

--- Demo02/src/main/java/demo/Animal.java
+++ Demo02/src/main/java/demo/Animal.java
@@ -29,10 +29,10 @@ public abstract class Animal implements Actions {
     @Override
     public void toFeed() throws PetGrewUpException, PetDiedException, PocheshiPuzikoException, PetWantsToPalyException, PetIsDirtyException, PetIsIllException, PetIsHungryException {
         changeHealthPoint(2, true);//add methods increase health
-        happiness += 1;
-        satiety += 10;
-        age += 0.2;
-        purity -= 2;
+        changeHappiness(1, true);
+        changeSatiety(10, true);
+        changeAge();
+        changePurity(2, false);
         check_stage(); //have to be static...
 
     }
@@ -45,13 +45,44 @@ public abstract class Animal implements Actions {
     @Override
     public void toScold() throws PetGrewUpException, PetDiedException, PocheshiPuzikoException, PetWantsToPalyException, PetIsDirtyException, PetIsIllException, PetIsHungryException {
         changeHealthPoint(2, false);
-        healthPoint -= 2;
-        happiness -= 2;
-        satiety -= 1;
-        age += 0.2;
+        changeHappiness(2, false);
+        changeSatiety(1, false);
+
+        changeAge();
         check_stage();
     }
 
+    @Override
+    public void toStroke() throws PetGrewUpException, PetDiedException, PocheshiPuzikoException, PetWantsToPalyException, PetIsDirtyException, PetIsIllException, PetIsHungryException {
+        changeHealthPoint(2, false);
+        changeHappiness(2, true);
+        changeSatiety(1, false);
+        changeAge();
+        check_stage();
+    }
+
+    @Override
+    public void toWash() throws PetGrewUpException, PetDiedException, PocheshiPuzikoException, PetWantsToPalyException, PetIsDirtyException, PetIsIllException, PetIsHungryException {
+        changeHealthPoint(2, true);
+        changeSatiety(1, false);
+        changeAge();
+        changePurity(3, true);
+        check_stage();
+    }
+
+    @Override
+    public void toHeal() throws PetGrewUpException, PetDiedException {
+
+    }
+
+    public void check_stage() throws PetDiedException, PetGrewUpException, PetIsHungryException, PetIsDirtyException, PetIsIllException, PetWantsToPalyException, PocheshiPuzikoException {
+        if (healthPoint <= MIN_HEALTH_POINT) {
+            throw new PetDiedException("Your pet is dead. Game over.");
+        }
+        if (age > MAX_AGE) {
+            throw new PetGrewUpException("Congratulations! Your pet has been grew up! \n Game over.");
+        }
+    }
     /**
      * This method changes field {@code healthPoint}.
      * If add true, digit will added to {@code healthPoint},
@@ -69,38 +100,61 @@ public abstract class Animal implements Actions {
         }
     }
 
-    @Override
-    public void toStroke() throws PetGrewUpException, PetDiedException, PocheshiPuzikoException, PetWantsToPalyException, PetIsDirtyException, PetIsIllException, PetIsHungryException {
-        changeHealthPoint(2, false);
-        happiness += 2;
-        satiety -= 1;
-        age += 0.2;
-        check_stage();
-    }
-
-    @Override
-    public void toWash() throws PetGrewUpException, PetDiedException, PocheshiPuzikoException, PetWantsToPalyException, PetIsDirtyException, PetIsIllException, PetIsHungryException {
-        changeHealthPoint(2, true);
-        satiety -= 1;
-        age += 0.2;
-        purity += 3;
-        check_stage();
-    }
-
-    @Override
-    public void toHeal() throws PetGrewUpException, PetDiedException {
-
-    }
-
-    public void check_stage() throws PetDiedException, PetGrewUpException, PetIsHungryException, PetIsDirtyException, PetIsIllException, PetWantsToPalyException, PocheshiPuzikoException {
-        if (healthPoint <= MIN_HEALTH_POINT) {
-            throw new PetDiedException("Your pet is dead. Game over.");
-        }
-        if (age > MAX_AGE) {
-            throw new PetGrewUpException("Congratulations! Your pet has been grew up! \n Game over.");
+    /**
+     * This method changes field {@code happiness}.
+     * If add true, digit will added to {@code happiness},
+     * else digit will subscribe
+     * @param digit Digit which need to add or subscribe
+     * @param add   True if need to add, False if need to subscribe
+     */
+    private void changeHappiness(int digit, boolean add) {
+        if (happiness < MAX_HAPPINESS) {
+            if (add) {
+                happiness += digit;
+            }else{
+                happiness -= digit;
+            }
         }
     }
 
+    /**
+     * This method changes field {@code satiety}.
+     * If add true, digit will added to {@code satiety},
+     * else digit will subscribe
+     * @param digit Digit which need to add or subscribe
+     * @param add   True if need to add, False if need to subscribe
+     */
+    private void changeSatiety(int digit, boolean add) {
+        if (happiness < MAX_SATIETY) {
+            if (add) {
+                satiety += digit;
+            }else{
+                satiety -= digit;
+            }
+        }
+    }
+    /**
+     * This method changes field {@code purity}.
+     * If add true, digit will added to {@code purity},
+     * else digit will subscribe
+     * @param digit Digit which need to add or subscribe
+     * @param add   True if need to add, False if need to subscribe
+     */
+    private void changePurity(int digit, boolean add) {
+        if (purity < MAX_PURITY) {
+            if (add) {
+                purity += digit;
+            }else{
+                purity -= digit;
+            }
+        }
+    }
+
+    private void changeAge() {
+        if (happiness < MAX_AGE) {
+                age += 0.2;
+        }
+    }
 
     public int getHealthPoint() {
         return healthPoint;
