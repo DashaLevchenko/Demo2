c7caf5345cc61722cec323faab4ca46b33430f19
QA engineer <kotarakh@gmail.com>
2020-03-16 22:04:59 +0200

    PetNeedsSomethingException added

--- Demo02/src/main/java/demo/PetIsDirtyException.java
+++
@@ -1,7 +0,0 @@
-package demo;
-
-public class PetIsDirtyException extends Exception {
-    PetIsDirtyException(String message) {
-        super(message);
-    }
-}
--- Demo02/src/main/java/demo/PocheshiPuzikoException.java
+++
@@ -1,7 +0,0 @@
-package demo;
-
-public class PocheshiPuzikoException extends Exception {
-    PocheshiPuzikoException(String message){
-        super(message);
-    }
-}
