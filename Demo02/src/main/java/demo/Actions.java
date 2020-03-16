031c49f1c957060db31fe2f15603315ec6b0cdaf
Dasha Levchenko <dashulalevchenko@gmail.com>
2020-03-16 17:40:06 +0200

    When we throw some exceptions in Animal class, we need to throw these exceptions in Actions interface

--- Demo02/src/main/java/demo/Actions.java
+++ Demo02/src/main/java/demo/Actions.java
@@ -2,9 +2,14 @@ package demo;
 
 public interface Actions {
     void toFeed() throws PetGrewUpException, PetDiedException;
+
     void toPlay();
-    void toScold();
-    void toStroke();
-    void toWash();
+
+    void toScold() throws PetGrewUpException, PetDiedException;
+
+    void toStroke() throws PetGrewUpException, PetDiedException;
+
+    void toWash() throws PetGrewUpException, PetDiedException;
+
     void toHeal();
 }
