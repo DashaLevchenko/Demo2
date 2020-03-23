e27f62b30e9530c4ec10b5f37fc3e5c4173560b9
Biskota <dbiskota@gmail.com>
2020-03-23 15:52:04 +0200

    pets methods

--- Demo02/src/main/java/demo/Cuckoo.java
+++ Demo02/src/main/java/demo/Cuckoo.java
@@ -1,15 +1,67 @@
 package demo;
 
-public class Cuckoo extends Animal{
+import demo.exceptions.PetDiedException;
+import demo.exceptions.PetGrewUpException;
+
+import java.util.Random;
+
+/**
+ * Class of pet.
+ */
+public class Cuckoo extends Animal {
+
+    Random random = new Random();
 
     public Cuckoo(String name) {
         super(name);
     }
-    public void makeSound() {
-        System.out.println("Cuckoooo");
-    }
-    public void flyAway() {
-        System.out.println("I was offended. Goodbye!");
+
+    /**
+     * Pet print it sound.
+     *
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String makeSound() throws PetGrewUpException, PetDiedException {
+        String sound = "Cuckoooo";
+        return sound;
     }
 
+    /**
+     * Pet run away.
+     *
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String flyAway() throws PetGrewUpException, PetDiedException {
+        String message = "I was offended. Goodbye!";
+        return message;
+    }
+
+    /**
+     * Ask pet how long are you live.
+     *
+     * @param age
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String countAge(int age) throws PetDiedException, PetGrewUpException {
+        String message = "You will lived: ";
+        age = age + random.nextInt(80);
+        return message + age;
+    }
+
+    /**
+     * Pet lay eggs, else make sound.
+     *
+     * @return
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String layAnEgg(boolean egg) throws PetGrewUpException, PetDiedException {
+        if (egg){
+            return flyAway();
+        }
+        return makeSound();
+    }
 }
