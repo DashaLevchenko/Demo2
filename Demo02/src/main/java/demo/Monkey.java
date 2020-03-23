5a48f64f6efdd7a6772823b968721c4375f2d3a2
Stanislaw <kutyaev.stass@gmail.com>
2020-03-23 21:28:11 +0200

    Merge branch 'master' of https://github.com/DashaLevchenko/Demo2 into New_pets
    # Conflicts:
    #	Demo02/src/main/java/demo/Animal.java

--- Demo02/src/main/java/demo/Monkey.java
+++ Demo02/src/main/java/demo/Monkey.java
@@ -3,19 +3,45 @@ package demo;
 import demo.exceptions.PetDiedException;
 import demo.exceptions.PetGrewUpException;
 
+/**
+ * Class of pet.
+ */
 public class Monkey extends Animal {
-   public Monkey(String name){
+    public Monkey (String name) {
         super(name);
     }
 
-
-    @Override
-    public String toPlay() throws PetGrewUpException, PetDiedException {
-        return super.toPlay();
+    /**
+     * Pet washes up.
+     *
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String toWashDishes() throws PetGrewUpException, PetDiedException {
+        String message = "Oh no, dirty dishes again!";
+        return message;
     }
 
-    @Override
-    public String toWash() throws PetDiedException, PetGrewUpException {
-        return super.toWash();
+
+    /*** Pet dances.
+     * *
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String toDance() throws PetGrewUpException, PetDiedException {
+        String message = "Everybody dance now!";
+        return message;
+    }
+
+    /**
+     * Pet eats.
+     *
+     * @return
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String toEat() throws PetGrewUpException, PetDiedException {
+        String message = "Mmm, time to fill in my belly";
+        return message;
     }
 }
--- Demo02/src/main/java/demo/Parrot.java
+++ Demo02/src/main/java/demo/Parrot.java
@@ -3,14 +3,44 @@ package demo;
 import demo.exceptions.PetDiedException;
 import demo.exceptions.PetGrewUpException;
 
+/**
+ * Class of pet.
+ */
 public class Parrot extends Animal {
-    public Parrot (String name){
+    public Parrot (String name) {
         super(name);
     }
-
-
-    @Override
-    public String toFeed() throws PetGrewUpException, PetDiedException {
-        return super.toFeed();
+    
+    /**
+     * Pet is sad.
+     *
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String toSing() throws PetGrewUpException, PetDiedException {
+        String message = "I feel sad. Time to sing!";
+        return message;
     }
+
+    
+    /*** Pet eats.
+     * *
+     * @throws PetGrewUpException
+     * @throws PetDiedException
+     */
+    public String toEat() throws PetGrewUpException, PetDiedException {
+        String message = "Something delicious smells... Is it my snacks?";
+        return message;
+        }
+        /**
+         * Pet flies.
+         *
+         * @return
+         * @throws PetGrewUpException
+         * @throws PetDiedException
+         */
+        public String toFly() throws PetGrewUpException, PetDiedException {
+            String message = "Hoorah, I am free!";
+            return message;
+        }
 }
